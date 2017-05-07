aa8e91db520bf636ec9391389390748a2c6236f8
Zachariasz <zah3@o2.pl>
2017-05-07 16:18:29 +0200

    added controller for contact form

--- src/OldMotorsBundle/Controller/ContactController.php
+++ src/OldMotorsBundle/Controller/ContactController.php
@@ -5,15 +5,66 @@ namespace OldMotorsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
     /**
-     * @Route("/contact")
+     * @Route("/contact"
+     * )
      * @Template()
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
+        $form = $this
+            ->createFormBuilder()
+            ->add('name', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'Imię i nazwisko',
+                    'pattern'     => '.{2,}' //minlength
+                )
+            ))
+            ->add('email', 'email', array(
+                'attr' => array(
+                    'placeholder' => 'Twój e-mail'
+                )
+            ))
+            ->add('subject', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'Temat wiadomości',
+                    'pattern'     => '.{3,}' //minlength
+                )
+            ))
+            ->add('message', 'textarea', array(
+                'attr' => array(
+                    'cols' => 90,
+                    'rows' => 10,
+                    'placeholder' => 'Treść wiadomości'
+                )
+            ))
+            ->add('Wyślij', 'submit')
+            ->getForm();
+        $form->handleRequest($request);
+
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            $data = $form->getData();
+            $message = \Swift_Message::newInstance()
+                ->setSubject($data['subject'])
+                ->setFrom('noreply@motors.com')
+                ->setTo($data['email'])
+                ->setBody(
+                    $this->renderView(
+                        'OldMotorsBundle:Email:contact.html.twig',
+                        array('message' => $data['message'],'from' => $data['email'])
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($message);
+        }
+                return array(
+            'form' => $form->createView()
+        );
 
     }
 
